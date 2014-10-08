91e5bafd7607e14813c5de3335cdd2c9d78c3549
Shopskasalata <shopskasalata@users.sourceforge.net>
2014-10-08 20:22:12 +0000

    new transfer rules

--- compile/test.sh
+++ compile/test.sh
@@ -1,4 +1,4 @@
-echo "I run home" | lt-proc eng-bis.automorf.bin | cg-proc -n -1 grammar.bin | \
+echo "I want to see my father's dog." | lt-proc eng-bis.automorf.bin | cg-proc -n -1 grammar.bin | \
   apertium-transfer ../transfer.t1x eng-bis.t1x.bin eng-bis.autobil.bin | \
   lt-proc -g bis-eng.autogen.bin
 
