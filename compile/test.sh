3c432622f54cb7f08a64071b1adbce07342a7006
Shopskasalata <shopskasalata@users.sourceforge.net>
2014-10-08 20:51:33 +0000

    added rule det + n = n + blong + det

--- compile/test.sh
+++ compile/test.sh
@@ -1,4 +1,4 @@
-echo "I want to see my father's dog." | lt-proc eng-bis.automorf.bin | cg-proc -n -1 grammar.bin | \
+echo "my island" | lt-proc eng-bis.automorf.bin | cg-proc -n -1 grammar.bin | \
   apertium-transfer ../transfer.t1x eng-bis.t1x.bin eng-bis.autobil.bin | \
   lt-proc -g bis-eng.autogen.bin
 
